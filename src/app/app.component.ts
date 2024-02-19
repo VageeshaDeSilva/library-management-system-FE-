c1087acd594f2dc61106e5d4cba2c6a0cb35302f
VageeshaDeSilva <suvadesilva@gmail.com>
2024-02-19 15:28:34 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavBarComponent } from './common/nav-bar/nav-bar.component';
+import { ViewAllBooksComponent } from './pages/view-all-books/view-all-books.component';
+import { SearchBookComponent } from './pages/search-book/search-book.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NavBarComponent,ViewAllBooksComponent,SearchBookComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
