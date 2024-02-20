45f7835a7188225b176ad96ab1d63306b0ceafa5
VageeshaDeSilva <suvadesilva@gmail.com>
2024-02-20 16:44:12 +0530

    done viewAllBooks page with deleteBtn (without editBtn)

--- src/app/pages/view-all-books/view-all-books.component.ts
+++ src/app/pages/view-all-books/view-all-books.component.ts
@@ -1,12 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-view-all-books',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, FormsModule, CommonModule],
   templateUrl: './view-all-books.component.html',
-  styleUrl: './view-all-books.component.css'
+  styleUrl: './view-all-books.component.css',
 })
-export class ViewAllBooksComponent {
+export class ViewAllBooksComponent implements OnInit {
+  private http;
+  public bookList: any = {};
+  private book: any;
 
+  constructor(private httpClient: HttpClient) {
+    this.http = httpClient;
+  }
+
+  ngOnInit(): void {
+    this.loadBooks();
+  }
+
+  nowBook(nowbook: any) {
+    this.book = nowbook;
+  }
+
+  loadBooks() {
+    this.http.get('http://localhost:8080/book/getAll').subscribe((data) => {
+      this.bookList = data;
+    });
+  }
+
+  removeBtnOnClick() {
+    this.http
+      .delete(`http://localhost:8080/book/deleteBook/${this.book.id}`, {
+        responseType: 'text',
+      })
+      .subscribe((data: string) => {
+        this.loadBooks();
+        this.book = null;
+      });
+  }
+
+  editBtnOnClick() {
+    alert('edit');
+  }
 }
